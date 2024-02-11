ffe5ff9e148b526117777afbfa95858596ea0968
Neha-p12 <nehapawara2779@gmail.com>
2024-02-12 00:41:39 +0530

    Update Main.java
    commit new

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main
 {
 public static void main(String[] args) {
-    System.out.println("Main");
+    System.out.println("Main class");
+    Student student=new Student();
+    student.sayHello();
 }
 }
